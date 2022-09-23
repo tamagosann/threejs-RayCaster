4e2cdc37447e81425b46d59290f72a4f95d12360
tamagosann <hssvs619@gmail.com>
2022-09-24 01:13:58 +0900

    Reset App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import React, { Suspense } from "react"
+import { Suspense } from "react"
 import { Canvas } from "@react-three/fiber"
-import { Floor } from "./components/Floor"
-import { Lights } from "./components/Lights"
-import { House } from "./components/House/House"
-import { OrbitControls } from "@react-three/drei"
-import { Graves } from "./components/Graves"
-import { Fog } from "./components/Fog"
-import { Ghosts } from "./components/Ghosts"
 
 function App() {
   return (
     <div>
-      <Canvas
-        shadows
-        dpr={[1, 2]}
-        camera={{ position: [0, 5, 15] }}
-        style={{ width: "100vw", height: "100vh" }}
-      >
-        <OrbitControls />
-        <Lights />
-        <Fog />
-        <Suspense fallback={null}>
-          <Ghosts />
-          <Floor />
-          <House />
-          <Graves />
-        </Suspense>
-        <axesHelper args={[10]} />
+      <Canvas dpr={[1, 2]}>
+        <Suspense fallback={null}></Suspense>
       </Canvas>
     </div>
   )
